ab362761b4a6fd8ddb2d3eecc977b90e88093263
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2019-10-12 19:09:23 -0500

    first try to login

--- js/index.js
+++ js/index.js
@@ -4,6 +4,17 @@ const userPhoto = document.getElementById('user-photo')
 function setUserInfo(user) {
   userBox.textContent = user.display_name
   userPhoto.src = user.image
+  lightdm.start_authentication(user.username);
+}
+
+function authentication_complete()
+{
+  if (lightdm.is_authenticated)
+    lightdm.login (lightdm.authentication_user, 'i3');
+  else {
+    console.log('error')
+  }
+
 }
 
 window.onload = function() {
