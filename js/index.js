996baddde2e48a1c75fd7242512d0fe93fc2dfa0
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2019-10-12 19:57:10 -0500

    it actually logins xD

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-const userBox = document.getElementById('user')
-const userPhoto = document.getElementById('user-photo')
+const inpUser = document.getElementById('user')
+const inpPassword = document.getElementById('password')
+const imgUserPhoto = document.getElementById('user-photo')
 
 function setUserInfo(user) {
-  userBox.textContent = user.display_name
-  userPhoto.src = user.image
+  inpUser.textContent = user.display_name
+  imgUserPhoto.src = user.image
   lightdm.start_authentication(user.username);
 }
 
-function authentication_complete()
-{
+function provideSecret() {
+  if (inpPassword.value) {
+    lightdm.provide_secret(inpPassword.value)
+  }
+}
+
+function authentication_complete() {
   if (lightdm.is_authenticated)
-    lightdm.login (lightdm.authentication_user, 'i3');
+    lightdm.login(lightdm.authentication_user, 'i3');
   else {
     console.log('error')
   }
-
 }
 
 window.onload = function() {
